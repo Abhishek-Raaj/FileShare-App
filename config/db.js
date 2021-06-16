960c2d15c066e4fa3b36abb4fb5779423f4c9dd1
Abhishek kumar <49031896+Abhishek-Raaj@users.noreply.github.com>
2021-06-17 03:19:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose");
 function connectDB()
 {
     const url=process.env.MONGO_CONNECTION_URL;
-//   const url="mongodb+srv://share:Okkl0NGc48x7K0QJ@cluster0.0iqnd.mongodb.net/fileshared?retryWrites=true&w=majority";
+
     mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }).
     then(()=>{
         console.log("database connected");
